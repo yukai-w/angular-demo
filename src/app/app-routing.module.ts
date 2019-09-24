87f1df580a75f147563763685152a5ec6e68a705
yukai-w <yukai.w@outlook.com>
2019-09-24 13:49:51 +0800

    add hero component
    官网示例heros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 // 路由配置
 const routes: Routes = [
   {
     path: '', component: TodoListComponent,
+  },
+  {
+    path: 'hero', component: HeroesComponent,
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
